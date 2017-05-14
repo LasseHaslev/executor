18b072bbb06004f5ac56a2eb7a90a3abe707fa6c
Lasse <lasse@haslev.no>
2017-05-14 08:48:08 +0200

    Added watch method.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
+import chokidar from 'chokidar';
 // console.log(fs);
 // import args from 'command-line-args';
 
 console.log('Hello world');
 
+console.log(process.cwd());
 
+var watcher = chokidar.watch('**/*.js', {
+  ignored: /(^|[\/\\])\../,
+  persistent: true
+});
+
+watcher.on( 'change', function( path, stats ) {
+    console.log(path);
+    console.log(path);
+} )
